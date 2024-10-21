c2215272af4281cf0d716c455783de8faf940631
Lulu Nabilah <lulunbila6604@gmail.com>
2024-10-21 18:44:10 +0700

    membuat view

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,2 +1,21 @@
-<h1>Hallo</h1>
-{{ dd($datasiswa) }}
+<h1>Data Siswa</h1>
+<table>
+    <tr>
+        <th>NAMA DEPAN</th>
+        <th>NAMA BELAKANG</th>
+        <th>JENIsiswa KELAMIN</th>
+        <th>AGAMA</th>
+        <th>ALAMAT</th>
+    </tr>
+
+    @foreach ($datasiswa as $siswa)
+    <tr>
+        <td>{{ $siswa->nama_depan }}</td>
+        <td>{{ $siswa->nama_belakang }}</td>
+        <td>{{ $siswa->jenisiswa_kelamin }}</td>
+        <td>{{ $siswa->agama }}</td>
+        <td>{{ $siswa->alamat }}</td>
+    </tr>
+    @endforeach
+   
+</table>
